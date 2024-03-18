32fe3b062ef8bebd1dfb496a3840eb629b5ee982
ShuKun Li <2562017477@qq.com>
2024-03-18 15:32:05 +0800

    课程计划排序

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/TeachPlanController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/TeachPlanController.java
@@ -1,14 +1,13 @@
 package com.xuecheng.content.api;
 
 import com.xuecheng.content.model.dto.TeachplanDto;
+import com.xuecheng.content.model.po.Teachplan;
 import com.xuecheng.content.service.TeachplanService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +24,25 @@ public class TeachPlanController {
     public List<TeachplanDto> getTreeNodes(@PathVariable Long courseId){
         return teachplanService.findTeachplanTree(courseId);
     }
+
+    @ApiOperation("课程计划创建或修改")
+    @PostMapping("/teachplan")
+    public void saveTeachplan(@RequestBody Teachplan teachplanDto) {
+
+    }
+
+
+    @ApiOperation("课程删除计划")
+    @DeleteMapping("/content/teachplan/{teachplanId}")
+    public void deleteTeachplan(@PathVariable Long teachplanId){
+        teachplanService.deleteTeachplan(teachplanId);
+    }
+
+    @ApiOperation("课程计划排序")
+    @PostMapping("/teach/{moveType}/{teachplanId}")
+    public void orderByTeachplan(@PathVariable String moveType,@PathVariable Long teachplanId){
+
+    }
+
+
 }
